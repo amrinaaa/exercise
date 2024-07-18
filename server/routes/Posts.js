14c34b42f152708852db58b294bb0a1d2c699f0a
amrina rossada <amrina0612@gmail.com>
2024-07-18 23:13:53 +0800

    update

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router()  ;
-const {Posts} = require ("../models");
+const router = express.Router();
+const { Posts } = require("../models");
 
-router.get("/", async (req, res) =>{
-    const listOfPosts = await Posts.findAll()
+router.get("/", async (req, res) => {
+    const listOfPosts = await Posts.findAll();
     res.json(listOfPosts);
 });
 
@@ -11,6 +11,6 @@ router.post("/", async (req, res) => {
     const post = req.body;
     await Posts.create(post);
     res.json(post);
-
 });
+
 module.exports = router;
