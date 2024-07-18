5fe08e5560a1807e3b0d5d5a533d1bf2cbbcb649
amrina rossada <amrina0612@gmail.com>
2024-07-18 17:29:58 +0800

    update get and posts

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
+
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3001")
+    console.log("Server is running on port 3001");
     });
 });
